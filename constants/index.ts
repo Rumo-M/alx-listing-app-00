21d13e4788ad70db26a7707b39da5ddffa0bccc5
Martha Maloba <marthamarumo92@gmail.com>
2025-07-06 22:55:32 +0200

    Updated the index.ts file under the constants folder

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+// App-wide constants
+
+export const SITE_NAME = 'ALX Listing App';
+
+export const DEFAULT_IMAGE = '/assets/default-image.jpg';
+
+export const NAV_LINKS = [
+  { name: 'Home', href: '/' },
+  { name: 'Listings', href: '/listings' },
+  { name: 'About', href: '/about' },
+  { name: 'Contact', href: '/contact' },
+];
